5e306ce478367da0af1a04eb9a5b3defdb8dd3c7
Taylor Smith <tsmith@tsmithcreative.com>
2021-02-08 12:40:28 -0600

    Add styled-components and throw up a basic css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { HeaderLogo } from './components';
+import { GlobalStyles, HeaderLogo } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HeaderLogo />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <div className="App">
+        <header className="App-header">
+          <HeaderLogo />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,86 @@
+import { createGlobalStyle } from 'styled-components';
+
+/**
+ * Starts with a CSS reset and then includes some global defaults. See
+ * https://github.com/jgthms/minireset.css/blob/master/minireset.css for
+ * the reset stuff.
+ */
+
+const GlobalStyles = createGlobalStyle`
+  html,
+  body,
+  p,
+  ol,
+  ul,
+  li,
+  dl,
+  dt,
+  dd,
+  blockquote,
+  figure,
+  fieldset,
+  legend,
+  textarea,
+  pre,
+  iframe,
+  hr,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    padding: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-size: 100%;
+    font-weight: normal;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button,
+  input,
+  select {
+    margin: 0;
+  }
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+
+  img,
+  video {
+    height: auto;
+    max-width: 100%;
+  }
+
+  iframe {
+    border: 0;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  td,
+  th {
+    padding: 0;
+  }
+`;
+
+export default GlobalStyles;
